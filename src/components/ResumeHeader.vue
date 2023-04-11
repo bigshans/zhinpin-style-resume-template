7db5e7a2af045a35c025b96f795a3452cf0dc16e
bigshans <26884666+bigshans@users.noreply.github.com>
2023-04-11 13:54:52 +0800

    fix: fix time compute

--- src/components/ResumeHeader.vue
+++ src/components/ResumeHeader.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
     import {useProps} from '@/utils';
-import { ref } from 'vue';
     import type {HeaderProps} from '../types/header.type';
     const props = defineProps<{ props: HeaderProps }>();
     const info = useProps(props, (props) => props.props);
     function getWorkExperience() {
         const start = new Date(info.value.workStart);
-        const now = new Date();
-        return now.getFullYear() - start.getFullYear();
+        const lastDay = info.value.lastDay && new Date(info.value.lastDay) || new Date();
+        return Math.ceil((lastDay.getTime() - start.getTime()) / 31536000000);
     }
-    function getAge(birthday: string)
+    function getAge()
     {
+        const birthday = info.value.birth;
         //出生时间 毫秒
         var birthDayTime = new Date(birthday).getTime(); 
         //当前时间 毫秒
         var nowTime = new Date().getTime(); 
         //一年毫秒数(365 * 86400000 = 31536000000)
-        return Math.ceil((nowTime-birthDayTime)/31536000000);
+        return Math.floor((nowTime - birthDayTime) / 31536000000);
     }
 </script>
 
@@ -24,7 +24,7 @@ import { ref } from 'vue';
     <div id="header">
         <div class="name">{{info.name}}</div>
         <p class="work-intent">
-            {{info.gender}} | 年龄：{{getAge(info.birth)}} | 电话：{{info.phone}} {{ info.wechat ? `|${info.wechat}` : '' }}
+            {{info.gender}} | 年龄：{{getAge()}} | 电话：{{info.phone}} {{ info.wechat ? `|${info.wechat}` : '' }}
             <br>
             {{getWorkExperience()}} 年工作经验 | 求职意向：{{info.workFor}} | 期望城市：{{info.workBase}}
         </p>
--- src/types/header.type.ts
+++ src/types/header.type.ts
@@ -2,6 +2,7 @@ export interface HeaderProps {
     name: string;
     gender: string;
     workStart: string; // 工作开始时间，将自动计算工作经验
+    lastDay?: string; // 最后一份工作截止时间
     workFor: string; // 求职意向
     workBase: string;
     phone: string;
